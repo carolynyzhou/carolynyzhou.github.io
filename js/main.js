445773e32dcc72b5e481b9c335630b930fcf3246
Carolyn <carolyn@Carolyns-MacBook-Pro.local>
2018-07-22 19:14:25 -0400

    First draft of portfolio

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// menu item Portfolio drop down submenu
+// $(document).ready(function() {
+//     var $sidebarArrow = $('.sidebar-menu-arrow');
+//     $sidebarArrow.click(function() {
+//         $(this).next().slideToggle(300);
+//     });
+// });
