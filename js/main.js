51fbc6cf13d8ad10d7c223ee2e29373260877323
Carolyn <carolyn@Carolyns-MacBook-Pro.local>
2019-06-18 22:38:48 -0400

    revamp

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// menu item Portfolio drop down submenu
-// $(document).ready(function() {
-//     var $sidebarArrow = $('.sidebar-menu-arrow');
-//     $sidebarArrow.click(function() {
-//         $(this).next().slideToggle(300);
-//     });
-// });
